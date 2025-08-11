09325bb5b034e827d8a4d77c4b36e7be7e7b1d85
Austin5788809-two <1335788809@qq.com>
2025-08-11 08:01:00 +0800

    完成（未改bug）

--- net.cpp
+++ net.cpp
@@ -12,6 +12,7 @@
 #include <unordered_map>
 #include <thread>
 #include <mutex>
+#include <sstream>
 
 std::mt19937 rng{std::random_device{}()};
 
@@ -26,11 +27,11 @@ double randfloat(double l, double r)
     return l + u * (r - l);
 }
 
-network::network(size_t lnum, const std::vector<size_t>& nnum) : layer_number(lnum), node_number(nnum)
+network::network(size_t lnum, const std::vector<size_t>& nnum, std::vector<int> af, std::vector<int> afd) : layer_number(lnum), node_number(nnum)
 {
     is_worked = false;
     // 不是我喜欢的参数，直接return
-    if (lnum != nnum.size() || lnum == 0)
+    if (lnum != nnum.size() || lnum != af.size() || lnum != afd.size() || lnum == 0)
         return;
     // 设定大小
     weight.resize(lnum - 1);
@@ -38,6 +39,8 @@ network::network(size_t lnum, const std::vector<size_t>& nnum) : layer_number(ln
     value.resize(lnum);
     act_f.resize(lnum);
     act_f_d.resize(lnum);
+    act_f = af;
+    act_f_d = afd;
     for (int i = 0; i < lnum; i++)
         bias.resize(nnum[i]),
         value.resize(nnum[i]);
@@ -54,28 +57,68 @@ network::network(size_t lnum, const std::vector<size_t>& nnum) : layer_number(ln
             for (int k = 0; k < nnum[i + 1]; k++)
                 // 初始化为随机值
                 weight[i][j][k] = randfloat(-1, 1);
-        // 隐藏层的激活函数为ReLU
-        act_f[i] = [](double x)->double{ return (x > 0) * x; };
-        // 隐藏层的激活函数导数为ReLU_d
-        act_f_d[i] = [](double x)->double{ return (x > 0); };
     }
-    // 输出层没有激活函数及其导数
-    act_f.back() = [](double x)->double{ return x; };
-    act_f_d.back() = [](double x)->double{ return 1; };
     // 创建成功
     is_worked = true;
 }
 
-void softmax(std::vector<double>& input)
+void active(std::vector<double>& value, int type) // 工具函数，激活一层
 {
-    double max_val = *std::max_element(input.begin(), input.end());
-    double sum = 0.0;
-    for (double& val : input)
-        sum += std::exp(val - max_val);
-    for (double& val : input)
-        val = std::exp(val - max_val) / sum;
+    switch (type)
+    {
+    case act_f_t::NONE:
+        break;
+    case act_f_t::RELU:
+        for (double& i : value)
+            i = std::max(i, 0.01 * i);
+        break;
+    case act_f_t::SIGMOID:
+        for (double& i : value)
+            i = 1 / (1 + exp(i));
+        break;
+    case act_f_t::SOFTMAX:
+        {
+            double sum = 0;
+            for (double i : value)
+                sum += exp(i);
+            for (double& i : value)
+                i = exp(i) / sum;
+            break;
+        }
+    default:
+        break;
+    }
 }
-void network::forward(const std::vector<double>& input, std::vector<double>& output, std::vector<double>& probs)
+
+void active_d(std::vector<double>& value, const std::vector<double>& g, int type) // 工具函数，激活导数一层
+{
+    switch (type)
+    {
+    case act_f_t::NONE:
+        break;
+    case act_f_t::RELU:
+        for (double& i : value)
+            i = (i >= 0) ? i : 0.01 * i;
+        break;
+    case act_f_t::SIGMOID:
+        for (double& i : value)
+            i = (1 / (1 + exp(i))) * (1 - 1 / (1 + exp(i)));
+        break;
+    case act_f_t::SOFTMAX:
+        {
+            double dot_gs = 0.0;
+            for (size_t i = 0; i < value.size(); i++)
+                dot_gs += g[i] * value[i];
+            for (size_t i = 0; i < value.size(); i++)
+                value[i] = value[i] * (g[i] - dot_gs);
+            break;
+        }
+    default:
+        break;
+    }
+}
+
+void network::forward(const std::vector<double>& input, std::vector<double>& output)
 {
     is_worked = false;
     // 不是我喜欢的参数，直接return
@@ -91,9 +134,9 @@ void network::forward(const std::vector<double>& input, std::vector<double>& out
             for (int k = 0; k < last_value.size(); k++) // 对于上一层所有节点的值（输入）
                 // 加权求和
                 value[i][j] += weight[i - 1][j][k] * last_value[k];
-            // 激活函数
-            value[i][j] = act_f[i](value[i][j]);
         }
+        // 激活函数
+        active(value[i], act_f[layer_number]);
         // 将last_value更新为当前层的value
         std::vector<double> temp(value[i]);
         std::swap(last_value, temp);
@@ -102,11 +145,6 @@ void network::forward(const std::vector<double>& input, std::vector<double>& out
     output.resize(value.back().size());
     for (int i = 0; i < value.back().size(); i++)
         output[i] = value.back()[i];
-    // 将softmax的结果复制到probs
-    softmax(value.back());
-    probs.resize(value.back().size());
-    for (int i = 0; i < value.back().size(); i++)
-        probs[i] = value.back()[i];
     // 执行成功
     is_worked = true;
 }
@@ -120,7 +158,7 @@ double network::get_loss(const std::vector<double>& input, const std::vector<dou
         return 0;
     std::vector<double> probs;
     // 前向传播得到结果
-    forward(input, output, probs);
+    forward(input, probs);
     // forward没执行成功
     if (!is_worked)
         return 0;
@@ -148,7 +186,7 @@ void network::backward(const std::vector<double>& input, const std::vector<doubl
         return;
     is_worked = false;
 
-    std::vector<std::vector<double>> delta; // 各层的误差delta
+    std::vector<std::vector<double>> delta(layer_number); // 各层的误差delta
     // 计算输出层误差
     delta.back().resize(node_number.back());
     for (size_t i = 0; i < node_number.back(); i++)
@@ -156,14 +194,17 @@ void network::backward(const std::vector<double>& input, const std::vector<doubl
     // 反向递推隐藏层误差
     for (int l = (int)layer_number - 2; l >= 0; l--)
     {
-        delta[l].assign(node_number[l], 0);
-        for (size_t j = 0; j < node_number[l]; l++)
+        delta[l].assign(node_number[l], 0.0);
+        // δ_l[i] = Σ_j δ_{l+1}[j] * w_l[i][j] * σ'_l(z_l[i])
+        for (size_t i = 0; i < node_number[l]; i++)
         {
-            for (size_t k = 0; k < node_number[l + 1]; k++)
-                delta[l][j] += weight[l][j][k] * delta[l + 1][k];
-            // 激活函数导数
-            delta[l][j] *= act_f_d[l](value[l][j]);
+            double sum = 0.0;
+            for (size_t j = 0; j < node_number[l + 1]; ++j)
+                sum += delta[l + 1][j] * weight[l][i][j];
+            delta[l][i] = sum;
         }
+        // 激活导数
+        active_d(delta[l], std::vector<double>(delta[l].size(), 1.0), act_f_d[l]);
     }
     // 更新权重和偏置
     for (size_t l = 1; l < layer_number; l++)
@@ -177,4 +218,72 @@ void network::backward(const std::vector<double>& input, const std::vector<doubl
         }
     // 执行成功
     is_worked = true;
+}
+
+template<typename T>
+void putline(std::ofstream& put, const std::vector<T>& v)
+{
+    std::copy(v.begin(), v.end(), std::ostream_iterator<T>(put, " "));
+    put << std::endl;
+}
+
+void network::save(const char* filename)
+{
+    is_worked = false;
+    std::ofstream put(filename);
+    // 不是我喜欢的文件名，直接______
+    if (!put.is_open())
+        return;
+    /**
+     *      规则
+     *  第一行一个layer_number
+     *  第二行共layer_number个数，代表node_number
+     *  接下来layer_number - 1组，每组node_number[i]行，每行node_number[i + 1]个数，代表weight
+     *  接下来layer_number行，每行node_number[i]个数，代表bias
+     *  接下来layer_number行，每行node_number[i]个数，代表value
+     *  然后一行layer_number个数，代表激活函数，具体映射：{0: 无, 1: ReLU, 2: sigmoid， 3: softmax}
+     *  然后一行layer_number个数，代表激活函数的导数
+     */
+    put << layer_number << std::endl;
+    putline(put, node_number);
+    for (int i = 0; i < layer_number - 1; i++)
+        for (int j = 0; j < node_number[i]; j++)
+            putline(put, weight[i][j]);
+    for (int i = 0; i < layer_number; i++)
+        putline(put, bias);
+    for (int i = 0; i < layer_number; i++)
+        putline(put, value);
+    putline(put, act_f);
+    putline(put, act_f_d);
+    is_worked = true;
+}
+
+template<typename T>
+void getline(std::ifstream& get, std::vector<T>& v)
+{
+    v.clear();
+    std::string line;
+    std::getline(get, line);
+    std::istringstream ss(line);
+    std::copy(std::istream_iterator<T>(ss), std::istream_iterator<T>(), std::back_inserter(v));
+}
+
+void network::load(const char* filename)
+{
+    is_worked = false;
+    std::ifstream get(filename);
+    // 不是我喜欢的文件名，直接______
+    if (!get.is_open())
+        return;
+    get >> layer_number;
+    getline(get, node_number);
+    for (int i = 0; i < layer_number - 1; i++)
+        for (int j = 0; j < node_number[i]; j++)
+            getline(get, weight[i][j]);
+    for (int i = 0; i < layer_number; i++)
+        getline(get, bias);
+    for (int i = 0; i < layer_number; i++)
+        getline(get, value);
+    getline(get, act_f);
+    getline(get, act_f_d);
 }
--- net.hpp
+++ net.hpp
@@ -4,22 +4,29 @@
 #include <stdexcept>
 #include <functional>
 #include <tuple>
-typedef std::vector<double> vec;
 
 double randfloat(double l, double r);
+
+enum act_f_t
+{
+    NONE,
+    RELU,
+    SIGMOID,
+    SOFTMAX
+};
 
 struct network
 {
     std::vector<std::vector<std::vector<double>>> weight; // weight[i][j][k]表示第i层第j个节点连接下一层第k个节点的权重
     std::vector<std::vector<double>> bias;                // bias[i][j]     表示第i层第j个节点的偏置
     std::vector<std::vector<double>> value;               // value[i][j]    表示第i层第j个节点的值
-    std::vector<std::function<double(double)>> act_f;     // act_f[i]       表示第i层使用的激活函数
-    std::vector<std::function<double(double)>> act_f_d;   // act_f[i]       表示第i层使用的激活函数的导数
+    std::vector<int> act_f;                               // act_f[i]       表示第i层使用的激活函数
+    std::vector<int> act_f_d;                             // act_f[i]       表示第i层使用的激活函数的导数
     size_t layer_number;                                  // layer_number   表示层数
     std::vector<size_t> node_number;                      // node_number[i] 表示第i层的节点数
     bool is_worked = false;                               // 记录每一次调用函数是否成功
-    network(size_t lnum, const std::vector<size_t>& nnum);
-    void forward(const std::vector<double>& input, std::vector<double>& output, std::vector<double>& prob);           // 前向传播, 返回输出层的值，用于测试
+    network(size_t lnum, const std::vector<size_t>& nnum, std::vector<int> af, std::vector<int> afd);
+    void forward(const std::vector<double>& input, std::vector<double>& prob);                                        // 前向传播, 返回输出层的值，用于测试
     double get_loss(const std::vector<double>& input, const std::vector<double>& label, std::vector<double>& output); // 前向传播，返回损失和输出，用于反向传播
     void backward(const std::vector<double>& input, const std::vector<double>& label);                                // 前向传播+反向传播，用于训练
     void save(const char* filename);
